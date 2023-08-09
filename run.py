d734472b806147261ec8f64aef6a75eb9782b1ad
NKotani <nanami.kotani@star.rcast.u-tokyo.ac.jp>
2023-08-09 15:32:26 -0700

    change comments to English

--- run.py
+++ run.py
@@ -95,24 +95,24 @@ for file in files:
     print(people[-1]) # the most hight confidence human:(0.885246, [0.0642671, 0.530506, 0.0191592, 0.0959821])
     high_person = people[-1]
     
-    pers_w = 432 # perspective画像の横幅
-    human_x = high_person[1][0] - 0.5 # 人間の中心のx座標(-0.5~0.5)
+    pers_w = 432 # width of perspective image
+    human_x = high_person[1][0] - 0.5 # x-coordinate of human center(-0.5~0.5)
     theta = int(human_x * 360)
-    human_y = 0.5 - high_person[1][1] # 人間の中心のy座標(-0.5~0.5)
+    human_y = 0.5 - high_person[1][1] # y-coordinate of human center(-0.5~0.5)
     phi = int(human_y * 180)
     
     human_w = high_person[1][2]*2
-    fov = int(human_w*360) # -120の間で可変
-    human_h = high_person[1][3]*1.2 # 人の縦幅の1.2倍
-    hfov = int(human_h*180) # 縦のfov(-150)
+    fov = int(human_w*360) # Variable below 120
+    human_h = high_person[1][3]*1.2 # 1.2 times the height of a person
+    hfov = int(human_h*180) # fov of vertical (-150)
     if fov > 90:
-        fov = 120 # 人物が大きい時YOLOが正確ではないので広めに揃える
-        if high_person[0] < 0.65: # confidenceが低い時
-            phi = 50 # 手はカメラの上を通過することが多い
-            hfov = 150 # 関節検出失敗したら人間と180度反対の部分を切り取る
+        fov = 120 # when the person is large, the YOLO is not accurate, so align it wider.
+        if high_person[0] < 0.65: # when confidence is low
+            phi = 50 # hands often pass over the camera
+            hfov = 150 # If joint detection fails, cut out the part of the body that is 180 degrees opposite to the human
     elif hfov > 150:
             hfov = 150
-    pers_h = int(pers_w * hfov / fov) # perspective画像の縦幅
+    pers_h = int(pers_w * hfov / fov) # hight of perspective image
 
     candidate, subset = np.empty((1,1)), np.empty(1)
     Test.write_stdout(f'横fov={fov}, 縦fov={hfov}', stdout, testResult_f)
@@ -136,15 +136,15 @@ for file in files:
     if saveimg:
         persImg = util.draw_bodypose(persImg, candidate, subset)
         output = os.path.join(jointPers_f,  sphere + '_' + str(theta) + '_' + str(phi) + '.png')
-        cv2.imwrite(output, persImg) # OpenPoseの結果
-        Test.draw_finger(joints, joints_num, output, jointPersFinger_f) # rightLeftArm_head_pytorchの結果
+        cv2.imwrite(output, persImg) # result of OpenPose
+        Test.draw_finger(joints, joints_num, output, jointPersFinger_f) # result of rightLeftArm_head_pytorch
     print(joints)
     print(msg)
     Test.write_stdout(msg, stdout, testResult_f)
     if joints_num == [-1]:
         sholder = [theta + 170, 50] 
         wrist = [theta + 170, 20]
-        vec = [0, -30] # 下向きに進む
+        vec = [0, -30] # go downwards
     else:
         vec = PointingVector.get_vec_mid(joints, joints_num, head, theta, phi, pers_w, pers_h, fov)
         wrist = np.rad2deg(P2E.get_angle([joints[joints_num[-1]][0], joints[joints_num[-1]][1]], theta, phi, pers_w, pers_h, fov)) # 指差している関節の一番端
@@ -153,49 +153,45 @@ for file in files:
     print(f'肩{sholder}手首{wrist}')
     z, y = [], []
 
-    if  saveimg: # 定性評価のとき
-        z, y = GC.get(sholder, wrist, file, greatOmni_f, True) # 要素数1080個→30度は90個分
-    else: # 定量評価
-        z, y = GC.get(sholder, wrist, file, save=False) # zは必ず返ってくる
-    if (np.isnan(y)).any():
-        Test.write_stdout(f'z={z},y={y}:GC計算失敗', stdout, testResult_f)
-        Test.write_stdout(f'{-3} {sphere}', 'score', testResult_f)
-        continue
+    if  saveimg:
+        z, y = GC.get(sholder, wrist, file, greatOmni_f, True) # Number of elements 1080 → 30 degrees for 90 elements
+    else:
+        z, y = GC.get(sholder, wrist, file, save=False) # z will always return
 
     rot_z = []
     rot_y = []
-    start_num = 0 # 手首の角度がz,yの何番目の要素か
-    last = 0 # 最後の要素の番号
+    start_num = 0 # what element of z/y is the angle of the wrist
+    last = 0 # number of the last element
     first, final = 0, 0
-    opposite = 0 # スタート地点の180度反対
-    if vec[0] == 0: # 周期60度
+    opposite = 0 # 180 degrees opposite of the starting point
+    if vec[0] == 0: # Period 60 degrees
         start_num = int((wrist[0]+175) * 1080 / 360) % 1080
         rot_z.append(int(z[start_num]))
         rot_y.append(int(y[start_num]))
         E2P.get_perspective(file, 60, rot_z[0], rot_y[0], 640, 640, os.path.join(outDirect_f,sphere+'/'))
         for i in [1,2,3,4]:
-            index = (start_num + i * 30) % 1080 # 10度ずつ進む
+            index = (start_num + i * 30) % 1080 # advance by 10 degrees
             rot_z.append(int(z[index]))
             rot_y.append(int(y[index]))
             E2P.get_perspective(file, 60, rot_z[i], rot_y[i], 640, 640, os.path.join(outDirect_f,sphere+'/'))
-        first = (start_num - 30) % 1080 # 10度は30個分
+        first = (start_num - 30) % 1080 #  10 degrees for 30 elements
         final = (start_num + 30*5) % 1080
     else:  
-        if vec[0] > 0: # 右向き
-            const = 30 # 指先が画像の右端にする
-            adjust = 27 # 最初は27度分ずらす
+        if vec[0] > 0: # right
+            const = 30 # fingertip should be on the right edge of the image.
+            adjust = 27 # shift it by 27 degrees at first
         else:
-            const = -30 # 指先が画像の左端
+            const = -30 # fingertip is on the left side of the image
             adjust = -27
-        for i in range(9): # 上端と下端に到達すると30°進まないので幅を持たせておく
+        for i in range(9): # keep it wide because it may not advance 30 degree
             if i == 0:
                 if wrist[0] < 180:
                     start_num = int((wrist[0]+180+adjust) * 1080 / 360) - 1
                 else:
                     start_num = int((wrist[0]-180+adjust) * 1080 / 360) - 1
                 start_num = start_num % 1080
-                if abs(y[start_num]-y[(start_num-const*3)%1080]) > 60: # 縦に大きく進む
-                    adjust = const * 12 / 30 # 12度分ずらす
+                if abs(y[start_num]-y[(start_num-const*3)%1080]) > 60: # proceed in a large, longitudinal direction
+                    adjust = const * 12 / 30 # shift by 12 degrees
                     start_num = int(start_num - const*1.5) % 1080 # 12=27-15
                 rot_z.append(int(z[start_num]))
                 rot_y.append(int(y[start_num]))
@@ -203,62 +199,61 @@ for file in files:
                 Test.write_stdout(f'z方向:{rot_z[i]}, y方向:{rot_y[i]}', stdout, testResult_f)
                 E2P.get_perspective(file, 60, rot_z[i], rot_y[i], 640, 640, os.path.join(outDirect_f,sphere+'/'))
             else:
-                last = (last + const * 3) % 1080 # i*90を+/-する
+                last = (last + const * 3) % 1080
                 opposite += 30
-                if abs(y[last]) > 60 and abs(rot_y[i-1]) > 60: # 天井か床が2回連続
-                    last = (last + const*2) % 1080 # 50度進む
+                if abs(y[last]) > 60 and abs(rot_y[i-1]) > 60: # ceiling or floor twice in a row
+                    last = (last + const*2) % 1080 # 50 degrees forward
                     opposite += 20
-                while abs(y[last]-rot_y[i-1]) >= 60: # 縦に大きく進まなくなるまでループ
-                    last = int(last - const*0.5) % 1080 # 5度ぶん減らす
+                while abs(y[last]-rot_y[i-1]) >= 60: # loop until no significant vertical progress is made.
+                    last = int(last - const*0.5) % 1080 # shift by 5 degrees
                     opposite -= 5
                 rot_z.append(int(z[last]))
                 rot_y.append(int(y[last]))
                 Test.write_stdout(f'z方向:{rot_z[i]}, y方向:{rot_y[i]}', stdout, testResult_f)
                 E2P.get_perspective(file, 60, rot_z[i], rot_y[i], 640, 640, os.path.join(outDirect_f,sphere+'/'))
-                if  opposite >= 180: # 指差している起点から左右180°反対まで
-                    print('左右180°過ぎました')
-                    Test.write_stdout('左右180°過ぎました', stdout, testResult_f)
+                if  opposite >= 180: # from the starting point you are pointing to the opposite side 180° to the left and right.
+                    print('Left or right 180° too far.')
+                    Test.write_stdout('Left or right 180° too far.', stdout, testResult_f)
                     break
      
         first = (start_num-const*3) % 1080
         final = (last+const*3) %1080
 
-    detected = [] # [切り取った画像数len(rot_z)][検出されたオブジェクト数]
+    detected = [] # [Number of cropped images len(rot_z)][Number of objects detected]
     for i in range(len(rot_z)):
         name = os.path.join(outDirect_f, sphere + '/' + sphere + '_' + str(int(rot_z[i])) + '_' + str(int(rot_y[i])) + '.jpg') # 'outDirect/'
         command = []
         if  saveimg:
-            command = ['python','yolov5/detect.py','--weights','yolov5/yolov5s.pt','--source',name,'--save-txt','--exist-ok','--save-conf','--project',obj_f,'--name', sphere, '--conf-thres', '0.28'] # 0.29以上のみ
-        else: # 定量評価
+            command = ['python','yolov5/detect.py','--weights','yolov5/yolov5s.pt','--source',name,'--save-txt','--exist-ok','--save-conf','--project',obj_f,'--name', sphere, '--conf-thres', '0.28'] # Only 0.29 or higher
+        else:
             command = ['python','yolov5/detect.py','--weights','yolov5/yolov5s.pt','--source',name,'--save-txt','--exist-ok','--save-conf','--project',obj_f,'--name', sphere, '--conf-thres', '0.28', '--nosave']
         proc = subprocess.run(command, capture_output=True)
     
         name_txt = os.path.join(obj_f,sphere+'/labels/' + sphere + '_' + str(int(rot_z[i])) + '_' + str(int(rot_y[i])) + '.txt') # 'obj/'+sphere+'/labels/'
         try:
             with open(name_txt, 'r') as f:
-                data = f.read().splitlines() # ラベル, xywh, conf(改行を除く)
+                data = f.read().splitlines() # label, xywh, conf(except \n)
         except FileNotFoundError as e:
-            data = [] # 何も検出されなかった
+            data = [] # nothing detected
         detected.append(data)
-        # logging.info(data)
     
-    # [切り取った画像数len(rot_z)][検出されたオブジェクト数][ラベル,[x,y,w,h],conf]
+    # [Number of cropped images len(rot_z)][Number of objects detected][label,[x,y,w,h],conf]
     obj = ROI.match_obj(detected, rot_z, rot_y, vec)
     result = ROI.distance_circle(obj, rot_z, rot_y, [z[first],y[first]], [z[final],y[final]])
     # print(result) # [(8.069252459121573,['chair', [2862, 1597, 545, 610], 0.57567]), ...]
     ground_truth = os.path.join(os.path.split(os.path.dirname(file))[0], 'ROI', sphere+'.txt')
     test_file = 'dummy'
-    if  saveimg: # 定性評価のとき
+    if  saveimg:
         # Test.crop_obj(obj, file)
         test_file = os.path.join(testResult_vec_f, sphere + '.jpg') # 'test_result/vec/' distance_circleでかいた画像
         Test.distance_circle(rot_z, rot_y, [z[first],y[first]], [z[final],y[final]], file, testResult_vec_f)
-        Test.roi2img(obj, test_file, testResult_distance_f) # 指差しベクトル自体はdistance_circleでかく
+        Test.roi2img(obj, test_file, testResult_distance_f) # the pointing vector itself is represented by a distance_circle.
     Correct.run(result, ground_truth, stdout, testResult_f, sphere, saveimg, test_file, theta)
-    # データセットの作成
+    # make dataset
     # score, _ = Correct.in_order_of_distance(result, ground_truth)
     # result = Data.make(result, theta, sphere, score, stdout)
 
-dt_now = datetime.datetime.now() # 実行終了時間
+dt_now = datetime.datetime.now() # run end time
 finish = dt_now.strftime('%Y%m%d-%H%M%S')
 Test.write_stdout(finish, stdout, testResult_f)
 Correct.cal_top1_5_10ALL(testResult_f, len(files))
